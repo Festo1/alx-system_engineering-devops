46d92cfd00339460f973459e44e67c6eb31d2bd8
Festo1 <festoeducation@gmail.com1>
2023-11-25 03:06:26 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#This code will install the packages puppet-lint
-package { 'flask':
-  ensure    =>  '2.1.0',
-  provider  =>  'pip3',
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
