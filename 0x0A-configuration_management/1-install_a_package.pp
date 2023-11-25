c2afc39604204852cf43188947103ac1803d3943
Festo1 <festoeducation@gmail.com1>
2023-11-25 03:46:06 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
-#!/usr/bin/pup
-# Using Puppet to install flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package {'flask':
-  ensure   =>  '2.1.0',
-  provider =>  'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'verify_flask_version':
+  command => '/usr/bin/flask --version',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
